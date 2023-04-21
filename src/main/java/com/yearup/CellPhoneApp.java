6a7e8e014626ed7a3ea336a7dd47142cc2764f43
10zinnyims <tnyima@appdev.yearup.org>
2023-04-21 12:45:30 -0400

    added files

---
+++ src/main/java/com/yearup/CellPhoneApp.java
@@ -0,0 +1,26 @@
+package com.yearup;
+
+public class CellPhoneApp {
+    public static void main(String[] args) {
+        CellPhone cellPhone1 = new CellPhone(1234567, "iPhone X", "AT&T", "800-555-5555", "Dana Wyatt");
+        CellPhone cellPhone2 = new CellPhone(2345678, "Samsung Galaxy S21", "Verizon", "855-555-2222", "John Smith");
+        CellPhone cellPhone3 = new CellPhone();
+
+        display(cellPhone1);
+        display(cellPhone2);
+        display(cellPhone3);
+
+        cellPhone1.dial(cellPhone2.getPhoneNumber());
+        cellPhone2.dial(cellPhone1.getPhoneNumber());
+        cellPhone3.dial(cellPhone1.getPhoneNumber());
+    }
+
+    public static void display(CellPhone phone) {
+        System.out.println("Serial Number: " + phone.getSerialNumber());
+        System.out.println("Model: " + phone.getModel());
+        System.out.println("Carrier: " + phone.getCarrier());
+        System.out.println("Phone Number: " + phone.getPhoneNumber());
+        System.out.println("Owner: " + phone.getOwner());
+    }
+}
+
